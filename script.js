eeb12d98f5a660480dcb9805c3b1e2632ca789ee
AnshikaMahajan8287 <anshikam8287@gmail.com>
2025-04-20 18:52:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+
+const h4all=document.querySelectorAll("#nav h4");
+h4all.forEach(function(elem){
+elem.addEventListener("mouseenter",function(){
+   
+    cursor.style.scale=3
+    cursor.style.border="1px solid #fff"
+    cursor.style.backgroundColor="transparent"
+
+});
+elem.addEventListener("mouseleave",function(){
+    cursor.style.scale=1
+    cursor.style.border="0px solid #95C11E"
+    cursor.style.backgroundColor="#95C11E";
+})
+})
+
+
+
+
+
+gsap.to("#nav",{
+  
+    backgroundColor:"	black",
+
+    duration:0.5,
+   height:"110px",
+   scrollTrigger:{
+    trigger:"#nav",
+    scroller:"body",
+    // markers:true,
+    start: "top top",
+    end: "bottom top", 
+    scrub:1
+   }
+
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
+
+gsap.from("#about-us img, #about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    // stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 58%",
+        scrub:3
+    }
+})
+
+gsap.from(".card",{
+   scale:0.8,
+    opacity:0,
+    duration:1,
+   
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:3
+    }
+})
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+gsap.from("#page4 h1",{
+    y:50,
+    duration:2,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        start:"top 75%",
+        end:"top 70%",
+        scrub:3
+}})
